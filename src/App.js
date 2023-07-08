494f29bcc7fc5078d63530f18a5bdd2bdfaa3f18
Anderson <andersonbarbosa.ti@gmail.com>
2023-07-08 19:12:54 -0300

    aa

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import { useCallback, useEffect, useState } from 'react'
+import { useCallback, useEffect, useState, useRef } from 'react'
 
 import { NavigationContainer } from '@react-navigation/native'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 import { useFonts, Roboto_400Regular, Roboto_700Bold  } from "@expo-google-fonts/roboto"
-
 import { StatusBar } from 'expo-status-bar'
+
 import * as SplashScreen from 'expo-splash-screen'
+import * as Notifications from 'expo-notifications'
 
 import { GlobalProvider } from './hooks/global'
 
 import { loadDatabase } from './database/connection'
+import { registerForPushNotificationsAsync } from './helpers/pushNotifications'
 
 // import './config/firebase'
 
@@ -18,8 +20,22 @@ import { COLORS } from './theme'
 
 SplashScreen.preventAutoHideAsync()
 
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false
+  })
+})
+
 export default function App() {
   const [isLoaded, setIsLoaded] = useState(false)
+  const [notification, setNotification] = useState(false)
+
+  const [expoPushToken, setExpoPushToken] = useState()
+
+  const responseListener = useRef()
+  const notificationListener = useRef()
 
   const [fontsLoaded] = useFonts({
     Roboto_400Regular, 
@@ -27,19 +43,20 @@ export default function App() {
   })
 
   useEffect(() => {
-    async function prepare() {
-      await loadDatabase()
+    loadDatabase().then(() => setIsLoaded(true))
+    registerForPushNotificationsAsync().then(token => setExpoPushToken(token))
 
-      setIsLoaded(true)
+    notificationListener.current = Notifications.addNotificationReceivedListener(notification => setNotification(notification))
+    responseListener.current = Notifications.addNotificationResponseReceivedListener(response => console.log(response))
+
+    return () => {
+      Notifications.removeNotificationSubscription(notificationListener.current)
+      Notifications.removeNotificationSubscription(responseListener.current)
     }
-
-    prepare()
   }, [])
 
   const onLayoutRootView = useCallback(async () => {
-    if (fontsLoaded && isLoaded) {
-      await SplashScreen.hideAsync()
-    }
+    if (fontsLoaded && isLoaded) await SplashScreen.hideAsync()
   }, [fontsLoaded, isLoaded])
 
   if (!fontsLoaded || !isLoaded) return null
---
+++ src/helpers/pushNotifications.js
@@ -0,0 +1,35 @@
+import * as Device from 'expo-device'
+import * as Notifications from 'expo-notifications'
+
+export async function registerForPushNotificationsAsync() {
+    if (!Device.isDevice) {
+      alert('Must use physical device for Push Notifications')
+  
+      return
+    }
+  
+    const { status: existingStatus } = await Notifications.getPermissionsAsync()
+    
+    if (existingStatus !== 'granted') {
+      const { status } = await Notifications.requestPermissionsAsync()
+
+      if (status !== 'granted') {
+        alert('Failed to get push token for push notification!')
+    
+        return
+      }
+    }
+  
+    const token = (await Notifications.getExpoPushTokenAsync()).data
+  
+    if (Platform.OS === 'android') {
+      Notifications.setNotificationChannelAsync('default', {
+        name: 'default',
+        importance: Notifications.AndroidImportance.MAX,
+        vibrationPattern: [0, 250, 250, 250],
+        lightColor: '#FF231F7C'
+      })
+    }
+  
+    return token
+}
